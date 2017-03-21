bfeac0d78386cfe654af05ba971ec0d148c48ce4
sirfreud <mikevaldez92@gmail.com>
2017-03-20 22:34:20 -0700

    Created timestamp and update fields for shortener model and deleted previous migrations

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class ShortenedURL(models.Model):
-    url = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=20)
+    url       = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=20, unique=True)
+    update    = models.DateTimeField(auto_now=True)  # auto_now means that every time the model is saved - it's going to set that time value
+    timestamp = models.DateTimeField(auto_now_add=True)  # sets time value when first created
 
     def __str__(self):
         return str(self.url)
