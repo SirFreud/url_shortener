08eb05b885b689c8829d07c001d5b9cd3e9f69bb
sirfreud <mikevaldez92@gmail.com>
2017-03-20 22:14:57 -0700

    specified default for shortcode, made and applied migrations

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ShortenedURL(models.Model):
     url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=20)
 
     def __str__(self):
         return str(self.url)
